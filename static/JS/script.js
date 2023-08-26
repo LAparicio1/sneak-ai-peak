3c8af86daff0f129d65f9f30b2b936e1ec472d34
LAparicio1 <126023411+LAparicio1@users.noreply.github.com>
2023-08-26 11:14:19 +0100

    Add files via upload

---
+++ static/JS/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("photoInput").addEventListener("change", function(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      document.getElementById("previewImage").setAttribute("src", e.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+});
